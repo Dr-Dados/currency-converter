1690f5e5fb8a79c996c393c11dc6335098a06897
Dr.Dados <adnane.bachchar@gmail.com>
2024-02-08 11:04:42 +0100

    adding API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [initialCurrency, setInitialCurrency] = useState("EUR");
+  const [convertedCurrency, setConvertedCurrency] = useState("USD");
+  const [converted, setConverted] = useState("");
+  useEffect(
+    function () {
+      async function converter() {
+        const host = "api.frankfurter.app";
+        const res = await fetch(
+          `https://${host}/latest?amount=${amount}&from=${initialCurrency}&to=${convertedCurrency}`
+        );
+        const data = await res.json();
+        setConverted(data.rates[convertedCurrency]);
+      }
+      converter();
+    },
+    [amount, initialCurrency, convertedCurrency]
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(+e.target.value)}
+      />
+      <select
+        value={initialCurrency}
+        onChange={(e) => setInitialCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={convertedCurrency}
+        onChange={(e) => setConvertedCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{converted} {convertedCurrency}</p>
     </div>
   );
 }
